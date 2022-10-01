bc1d360ffe926e07a8540ceff91e619cb2d80d8b
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-10-01 18:00:14 -0300

    feat: change common input to input component

--- pages/components/SearchByProduct/index.tsx
+++ pages/components/SearchByProduct/index.tsx
@@ -1,9 +1,19 @@
+import { useState } from 'react';
+import Input from '../Input';
 import styles from './styles.module.css'
 
 export default function SearchByProduct() {
+  const [searchedProduct, setSearchedProduct] = useState(``)
+
   return (
     <div className={styles.searchByProductContainer}>
-      <input className={styles.searchByProductInput} type="text" placeholder="Pesquise um produto na lista" />
+      <Input
+        type="text"
+        name="searchedProduct"
+        value={searchedProduct}
+        placeholder="Pesquise um produto na lista"
+        onChange={setSearchedProduct}
+      />
       <button type="button" className={styles.searchByProductButton}>Buscar</button>
     </div>
   );
