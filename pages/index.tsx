e0077a8c6265927e62597c7489d2fa74cf2749d9
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-10-01 11:39:50 -0300

    feat: add component SearchByProduct

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,15 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Header from './components/Header'
+import SearchByProduct from './components/SearchByProduct'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
       <Header />
+      <div className={styles.homePageContent}>
+        <SearchByProduct />
+      </div>
     </div>
   )
 }
