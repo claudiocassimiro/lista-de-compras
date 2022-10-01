0df41bbc073218a9d1a81cdfa97394067bd29ebf
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-10-01 11:05:59 -0300

    feat: add Header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,11 @@
 import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
+import Header from './components/Header'
 
 const Home: NextPage = () => {
   return (
     <div className={styles.container}>
-      
+      <Header />
     </div>
   )
 }
