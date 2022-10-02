48d79d6286ee21549bbda412e48264fe53945979
cassimiroclaudio <claudio.cassimiro@imobles.com>
2022-10-02 17:09:11 -0300

    feat: hotfix

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ class MyDocument extends Document {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
           <link rel="manifest" href="/manifest.json" />
-          <link rel="apple-touch-icon" href="/icon-192x192.png" />
+          <link rel="apple-touch-icon" href="/icon-256x256.png" />
           <meta name="theme-color" content="#fff" />
         </Head>
         <body>
