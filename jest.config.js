f89e605ce0b74a1a8c87f9bc0a994d2cad066a04
claudiocassimiro <days_surfing@live.com>
2022-10-21 14:58:54 -0300

    feat: add transform to use swc

--- jest.config.js
+++ jest.config.js
@@ -7,5 +7,8 @@ const customJestConfig = {
   moduleDirectories: ['node_modules', '<rootDir>/'],
   testEnvironment: 'jest-environment-jsdom',
   setupFilesAfterEnv: ['@testing-library/jest-dom/extend-expect'],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['next/babel'] }],
+  },
 };
 module.exports = createJestConfig(customJestConfig);
