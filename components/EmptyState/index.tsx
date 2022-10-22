fdb07413edbb300c52a7e725030ae6439d170a7f
arthurlemos <arthurlemos600@gmail.com>
2022-10-22 17:25:51 -0300

    feat: implement the component empty state

---
+++ components/EmptyState/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import styles from './styles.module.css';
+
+export default function EmptyState() {
+  return (
+    <div className={styles.emptyStateContainer}>
+      <div className={styles.emptyStateContainerImage}>
+        <Image
+          src="/images/empty-state-image.webp"
+          alt="Picture of the author"
+          layout="fill"
+        />
+      </div>
+      <p className={styles.emptyStateText}>
+        Seu carrinho está vazio, Adicione alguns Produtos!
+      </p>
+    </div>
+  );
+}
