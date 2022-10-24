d0c9a55f08129354a368b02a9f1552a0a800701f
claudiocassimiro <days_surfing@live.com>
2022-10-24 17:20:53 -0300

    refactor: separete logic to render filteredProducts to another component

--- components/RenderProducts/index.tsx
+++ components/RenderProducts/index.tsx
@@ -4,60 +4,38 @@ import styles from './styles.module.css';
 
 export interface RenderProductsProps {
   products: Product[];
-  filteredProduct: Product[];
   handleRemoveProduct: (index: number) => void;
 }
 
 export default function RenderProducts({
   products,
-  filteredProduct,
   handleRemoveProduct,
 }: RenderProductsProps) {
   return products?.length > 0 ? (
     <div className={styles.renderProductsContainer}>
-      {filteredProduct?.length > 0
-        ? filteredProduct?.map((product, index) => (
-            <div
-              data-testid="filtered-products"
-              className={styles.renderProductsProductWrapper}
-              key={`${product?.productName}-${index + 1}`}
-            >
-              <div className={styles.renderProductsProductContent}>
-                <p className={styles.renderProductsProductName}>
-                  {product?.productName}
-                </p>
-                <hr className={styles.divider} />
-                <p className={styles.renderProductsProductPrice}>
-                  {product?.productPrice}
-                </p>
-              </div>
-              <AiOutlineCloseCircle
-                size="25"
-                onClick={() => handleRemoveProduct(index)}
-              />
+      <div className={styles.renderProductsContainer}>
+        {products?.map((product, index) => (
+          <div
+            data-testid="filtered-products"
+            className={styles.renderProductsProductWrapper}
+            key={`${product?.productName}-${index + 1}`}
+          >
+            <div className={styles.renderProductsProductContent}>
+              <p className={styles.renderProductsProductName}>
+                {product?.productName}
+              </p>
+              <hr className={styles.divider} />
+              <p className={styles.renderProductsProductPrice}>
+                {product?.productPrice}
+              </p>
             </div>
-          ))
-        : products?.map((product, index) => (
-            <div
-              data-testid="products"
-              className={styles.renderProductsProductWrapper}
-              key={`${product?.productName}-${index + 1}`}
-            >
-              <div className={styles.renderProductsProductContent}>
-                <p className={styles.renderProductsProductName}>
-                  {product?.productName}
-                </p>
-                <hr className={styles.divider} />
-                <p className={styles.renderProductsProductPrice}>
-                  {product?.productPrice}
-                </p>
-              </div>
-              <AiOutlineCloseCircle
-                size="25"
-                onClick={() => handleRemoveProduct(index)}
-              />
-            </div>
-          ))}
+            <AiOutlineCloseCircle
+              size="25"
+              onClick={() => handleRemoveProduct(index)}
+            />
+          </div>
+        ))}
+      </div>
     </div>
   ) : null; // TODO: render empty page
 }
