cc3205d2f7375c60c7983b4798030ff372a71d74
claudiocassimiro <days_surfing@live.com>
2022-10-29 19:37:19 -0300

    refactor: change message of NotFound component

--- components/NotFound/index.tsx
+++ components/NotFound/index.tsx
@@ -1,31 +1,8 @@
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
-import { selectSearchedProduct } from '../../store/searchProductsSlice';
 import styles from './styles.module.css';
 
 export default function NotFound() {
-  const [isLoading, setIsLoading] = useState(true);
-  const term = useSelector(selectSearchedProduct);
-
-  const cancelTimeout = setTimeout(() => {
-    setIsLoading(false);
-  }, 3000);
-
-  useEffect(
-    () => () => {
-      clearTimeout(cancelTimeout);
-    },
-    [cancelTimeout],
-  );
-
-  return isLoading ? (
-    <div className={styles.notFoundContainerSpinner}>
-      <div className={styles.notFoundSpinner}>
-        <div className={styles.notFoundSpinnerHead} />
-      </div>
-    </div>
-  ) : (
+  return (
     <div className={styles.notFoundContainer}>
       <div className={styles.notFoundContainerImage}>
         <Image
@@ -34,7 +11,9 @@ export default function NotFound() {
           layout="fill"
         />
       </div>
-      <p className={styles.notFoundText}>{term} ainda não está na sua lista</p>
+      <p className={styles.notFoundText}>
+        Este item ainda não está na sua lista
+      </p>
     </div>
   );
 }
