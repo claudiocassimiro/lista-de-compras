9aaff2df09586e03c7bd582fd9dd84a384a5f4b0
claudiocassimiro <days_surfing@live.com>
2022-11-01 15:34:49 -0300

    feat: change header text

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -3,7 +3,7 @@ import styles from './styles.module.css';
 export default function Header() {
   return (
     <header className={styles.header}>
-      <h1 className={styles.title}>Lista de Compras</h1>
+      <h1 className={styles.title}>Meu Carrinho</h1>
     </header>
   );
 }
