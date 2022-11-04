c4a4d7572448975617911bdca4538fe4c4e7d07a
claudiocassimiro <days_surfing@live.com>
2022-11-04 11:27:27 -0300

    feat: add a meta description on page

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,10 @@ class MyDocument extends Document {
           <link rel="manifest" href="/manifest.json" />
           <link rel="apple-touch-icon" href="/icon-512x512.png" />
           <meta name="theme-color" content="#fff" />
+          <meta
+            name="description"
+            content="Meu Carrinho é um aplicativo de ofertas em estabelecimentos parceiros e também serve como lista de compras. Economize comprando, compre economizando e acompanhe o valor total do seu carrinho"
+          />
         </Head>
         <body>
           <Main />
