80436219746c9f1d2c82bd8ee7c30746fa749e70
claudiocassimiro <days_surfing@live.com>
2022-11-04 16:44:59 -0300

    fix: remove glob and add fs

--- pages/sitemap.xml.tsx
+++ pages/sitemap.xml.tsx
@@ -1,41 +1,49 @@
 import { GetServerSideProps } from 'next';
-
-const glob = require(`glob`);
+import * as fs from 'fs';
 
 export default function Sitemap() {
   return null;
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ res }) => {
-  const pagesDir = `pages/**/*.tsx`;
-  let pagesPaths = await glob.sync(pagesDir);
+  const BASE_URL = `https://meucarrinho.vercel.app/`;
 
-  pagesPaths = pagesPaths
-    .filter((path: string) => !path.includes(`[`))
-    .filter((path: string) => !path.includes(`/_`))
-    .filter((path: string) => !path.includes(`404`));
+  const staticPaths = fs
+    .readdirSync(`pages`)
+    .filter(
+      (staticPage) =>
+        ![
+          `api`,
+          `_app.tsx`,
+          `_document.tsx`,
+          `404.tsx`,
+          `sitemap.xml.tsx`,
+        ].includes(staticPage),
+    )
+    .map((staticPagePath) => `${BASE_URL}/${staticPagePath}`);
 
-  const allPaths = [...pagesPaths];
+  const allPaths = [...staticPaths]; // if in the future has dinamic pages, it will goes here check this link: https://enlear.academy/how-to-create-sitemaps-with-nextjs-668da9601a03
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset
-        xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-        xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-              http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    <urlset
+          xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+          xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+          xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+                http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">
+    
+        ${allPaths
+          .map(
+            (url) => `
+              <url>
+                <loc>${url}</loc>
+                <lastmod>${new Date().toISOString()}</lastmod>
+              </url>
+            `,
+          )
+          .join(``)}
 
-  ${allPaths
-    .map(
-      (url) => `
-        <url>
-          <loc>${url}</loc>
-          <lastmod>${new Date().toISOString()}</lastmod>
-        </url>
-      `,
-    )
-    .join(``)}
-  
-  </urlset>`;
+    </urlset>
+  `;
 
   res.setHeader(`Content-Type`, `text/xml`);
   res.write(sitemap);
